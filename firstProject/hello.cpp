8b1cbf950776d7e04c44ac96412ff42d585d5317
boppy64 <maxwestman3@gmail.com>
2025-02-08 20:33:36 +0200

    fixed branch I think

---
+++ firstProject/hello.cpp
@@ -0,0 +1,16 @@
+/*
+* hello.cpp
+* Prints out my name,
+* hometown,
+* and hobby
+*/
+#include <cstdio>
+#include <iostream>
+
+int main() {
+    printf("Hello, my name is Max :O\n");
+    printf("My home town is Jyvaskyla\n");
+    printf("My favorite hobby is languages\n");
+    std::cout << "I like printing this way so much more!" << std::endl;
+}//This is a comment -- Why is it grey in vs lol
+/*Why does it look like this in vs as well lol -- hello.cpp, prints stuff, by max */
